5cdf2931528221a08435cbb372e3392f1065a5ba
Jamie Dawson <jamierobertdawson@gmail.com>
2024-07-15 22:58:17 -0700

    basic POST request works for backend with MongoDB. I need to update my code so it's just updating one object and not generating new objects

--- spotify-app-backend/index.js
+++ spotify-app-backend/index.js
@@ -1,8 +1,13 @@
-import { PORT, mongoDBURL } from "./config.js";
 import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
+import { Albums } from "./models/albumsModel.js";
 
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -19,3 +24,30 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+app.post("/albums", async (request, response) => {
+  try {
+    if (
+      !request.body.one ||
+      !request.body.two ||
+      !request.body.three ||
+      !request.body.four
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: one, two, three, and four",
+      });
+    }
+    const newAlbum = {
+      one: request.body.one,
+      two: request.body.two,
+      three: request.body.three,
+      four: request.body.four,
+    };
+    const album = await Albums.create(newAlbum);
+
+    return response.status(201).send(album);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
